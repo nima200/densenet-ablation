1bd62ac04e1cc303809f4fc0cb9e07b70480bb16
James Brace <bracejames95@gmail.com>
2018-04-18 18:09:02 +0000

    make changes to run scripts

--- src/run_james_stuff.sh
+++ src/run_james_stuff.sh
@@ -4,13 +4,13 @@
 source activate base
 
 # Run Composite Function Permutations
-python3 cifar10/CompositeFunctions/cifar10.py
+python3 cifar10/CompositeFunctions/cifar10.py true
 
 # Run Regularization tests
-python3 cifar10/FashionMNIST/fashion_mnist.py
+python3 cifar10/FashionMNIST/fashion_mnist.py true
 
 #Run Fashion MNIST
-python3 FashionMNIST/fashion_mnist.py
+python3 FashionMNIST/fashion_mnist.py true
 
 
 
