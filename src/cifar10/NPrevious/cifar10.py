a681f014141a3bf5a20d32f2533899c04ebb1924
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2018-04-18 05:56:04 +0000

    Made a loop to test 5 models with different n-previous

--- src/cifar10/NPrevious/cifar10.py
+++ src/cifar10/NPrevious/cifar10.py
@@ -27,84 +27,91 @@ growth_rate = 12
 nb_filter = -1
 dropout_rate = 0.0  # 0.0 for data augmentation
 
-if len(sys.argv) > 1:
-    augment = sys.argv[1]
-else:
-    augment = 'false'
+bottleneck = False
 
-load_models = False
-if len(sys.argv) > 2:
-    assert sys.argv[2] == '--load_models', 'Unknown flag: ' + sys.argv[2]
-    print("Loading models from file")
-    load_models = True
+for nb_previous_used in range(1,6):
 
-model = densenet.DenseNet(img_dim, classes=nb_classes, depth=depth, nb_dense_block=nb_dense_block,
-                          growth_rate=growth_rate, nb_filter=nb_filter, dropout_rate=dropout_rate, weights=None,
-                          use_all_previous=False, nb_previous_used=5)
-print("Model created")
+    model_name = "DenseNet-40-12-CIFAR10-nbPrev="+str(nb_previous_used)
 
-model.summary()
-optimizer = Adam(lr=1e-3)  # Using Adam instead of SGD to speed up training
-model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=["accuracy"])
-print("Finished compiling")
-print("Building model...")
-
-(trainX, trainY), (testX, testY) = cifar10.load_data()
-
-trainX = trainX.astype('float32')
-testX = testX.astype('float32')
-
-trainX = densenet.preprocess_input(trainX)
-testX = densenet.preprocess_input(testX)
-
-Y_train = np_utils.to_categorical(trainY, nb_classes)
-Y_test = np_utils.to_categorical(testY, nb_classes)
-
-generator = ImageDataGenerator(rotation_range=15,
-                               width_shift_range=5. / 32,
-                               height_shift_range=5. / 32,
-                               horizontal_flip=True)
-
-generator.fit(trainX, seed=0)
-
-# Load model
-weights_file = "weights/DenseNet-40-12-CIFAR10.h5"
-
-if os.path.exists(weights_file) and load_models:
-    model.load_weights(weights_file, by_name=True)
-    print("Model loaded.")
-
-out_dir = "weights/"
-
-lr_reducer = ReduceLROnPlateau(monitor='val_acc', factor=np.sqrt(0.1),
-                               cooldown=0, patience=5, min_lr=1e-5)
-model_checkpoint = ModelCheckpoint(weights_file, monitor="val_acc", save_best_only=True,
-                                   save_weights_only=True, verbose=1)
-
-csv = CSVLogger("Densenet-40-12-CIFAR10.csv", separator=',')
-
-callbacks = [lr_reducer, model_checkpoint, csv]
-try:
-    if augment == 'true':
-        print("Training with data augmentation...")
-        model.fit_generator(generator.flow(trainX, Y_train, batch_size=batch_size),
-                            steps_per_epoch=len(trainX) // batch_size, epochs=nb_epoch,
-                            callbacks=callbacks,
-                            validation_data=(testX, Y_test),
-                            validation_steps=testX.shape[0] // batch_size, verbose=1)
+    if len(sys.argv) > 1:
+        augment = sys.argv[1]
     else:
-        print("Training without data augmentation...")
-        model.fit(trainX, Y_train, batch_size=batch_size, epochs=nb_epoch, callbacks=callbacks,
-                  validation_data=(testX, Y_test), verbose=2)
-except KeyboardInterrupt:
-    print("Training interrupted")
-    sys.exit(1)
+        augment = 'false'
 
-yPreds = model.predict(testX)
-yPred = np.argmax(yPreds, axis=1)
-yTrue = testY
+    load_models = False
+    if len(sys.argv) > 2:
+        assert sys.argv[2] == '--load_models', 'Unknown flag: ' + sys.argv[2]
+        print("Loading models from file")
+        load_models = True
 
-accuracy = metrics.accuracy_score(yTrue, yPred) * 100
-error = 100 - accuracy
-print("Accuracy : ", accuracy)
-print("Error : ", error)
+    model = densenet.DenseNet(img_dim, classes=nb_classes, depth=depth, nb_dense_block=nb_dense_block,
+                              growth_rate=growth_rate, nb_filter=nb_filter, dropout_rate=dropout_rate, weights=None,
+                              bottleneck=bottleneck,
+                              use_all_previous=False, nb_previous_used=nb_previous_used)
+    print("Model created")
+
+    model.summary()
+    optimizer = Adam(lr=1e-3)  # Using Adam instead of SGD to speed up training
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=["accuracy"])
+    print("Finished compiling")
+    print("Building model...")
+
+    (trainX, trainY), (testX, testY) = cifar10.load_data()
+
+    trainX = trainX.astype('float32')
+    testX = testX.astype('float32')
+
+    trainX = densenet.preprocess_input(trainX)
+    testX = densenet.preprocess_input(testX)
+
+    Y_train = np_utils.to_categorical(trainY, nb_classes)
+    Y_test = np_utils.to_categorical(testY, nb_classes)
+
+    generator = ImageDataGenerator(rotation_range=15,
+                                   width_shift_range=5. / 32,
+                                   height_shift_range=5. / 32,
+                                   horizontal_flip=True)
+
+    generator.fit(trainX, seed=0)
+
+    # Load model
+    weights_file = "weights/"+model_name+ ".h5"
+
+    if os.path.exists(weights_file) and load_models:
+        model.load_weights(weights_file, by_name=True)
+        print("Model loaded.")
+
+    out_dir = "weights/"
+
+    lr_reducer = ReduceLROnPlateau(monitor='val_acc', factor=np.sqrt(0.1),
+                                   cooldown=0, patience=5, min_lr=1e-5)
+    model_checkpoint = ModelCheckpoint(weights_file, monitor="val_acc", save_best_only=True,
+                                       save_weights_only=True, verbose=1)
+
+    csv = CSVLogger(model_name+".csv", separator=',')
+
+    callbacks = [lr_reducer, model_checkpoint, csv]
+    try:
+        if augment == 'true':
+            print("Training with data augmentation...")
+            model.fit_generator(generator.flow(trainX, Y_train, batch_size=batch_size),
+                                steps_per_epoch=len(trainX) // batch_size, epochs=nb_epoch,
+                                callbacks=callbacks,
+                                validation_data=(testX, Y_test),
+                                validation_steps=testX.shape[0] // batch_size, verbose=1)
+        else:
+            print("Training without data augmentation...")
+            model.fit(trainX, Y_train, batch_size=batch_size, epochs=nb_epoch, callbacks=callbacks,
+                      validation_data=(testX, Y_test), verbose=2)
+    except KeyboardInterrupt:
+        print("Training interrupted")
+        sys.exit(1)
+
+    yPreds = model.predict(testX)
+    yPred = np.argmax(yPreds, axis=1)
+    yTrue = testY
+
+    accuracy = metrics.accuracy_score(yTrue, yPred) * 100
+    error = 100 - accuracy
+    print("Accuracy : ", accuracy)
+    print("Error : ", error)
