300cd3eb1bc92051b7c25f63e488e9abc10180f7
Nima Adibpour <nima.adibpour@mail.mcgill.ca>
2018-04-19 21:00:32 -0400

    Add dropout tests to script

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -6,6 +6,9 @@ source activate base
 # Run Composite Function Permutations
 #python3 cifar10/CompositeFunctions/cifar10.py 2>&1 | tee -a cifar10/CompositeFunctions/log
 
+# Run Dropout tests
+python3 cifar10/Dropout/cifar10.py 2>&1 | tee -a cifar10/Dropout/log
+
 # Run Regularization tests
 python3 cifar10/Regularization/cifar10.py 2>&1 | tee -a cifar10/Regularization/log
 
